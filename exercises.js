0d9274f23a1a0853f9d111c94f71359596eb7cb7
PD135054 <adminstrator@Adminstrators-MacBook-Pro.local>
2017-03-16 22:42:44 +0100

    Camillas files

--- exercises.js
+++ exercises.js
@@ -2,92 +2,152 @@
 
 //Do not change any of the function names
 
+//ok
 function makeCat(name, age) {
+    var newCat = {
+      name: name,
+      age: age,
+      meow: function() {
+        return 'Meow!';
+      }
+    };
+    return newCat;
+  }
   //create a new object with a name property with the value set to the name argument
   //add an age property to the object with the value set to the age argument
   //add a method called meow that returns the string 'Meow!'
   //return the object
-}
 
+
+//ok
 function addProperty(object, property) {
+    object[property] = null;
+    return object;
   //add the property to the object with a value of null
   //return the object
   //note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
 }
 
+//ok
 function invokeMethod(object, method) {
+    object[method]();
   //method is a string that contains the name of a method on the object
   //invoke this method
   //nothing needs to be returned
 }
 
+//ok
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
+    return mysteryNumberObject.mysteryNumber * 5;
   //mysteryNumberObject has a property called mysteryNumber
   //multiply the mysteryNumber property by 5 and return the product
 }
 
+//ok
 function deleteProperty(object, property) {
+    delete object[property];
+    return object;
   //remove the property from the object
   //return the object
 }
 
+//ok
 function newUser(name, email, password) {
+    return new Object({
+        name: name,
+        email: email,
+        password: password
+    });
   //create a new object with properties matching the arguments passed in.
   //return the new object
 }
 
+//
 function hasEmail(user) {
+    return user.email ? true : false;
   //return true if the user has a value for the property 'email'
   //otherwise return false
 }
 
+//ok
 function hasProperty(object, property) {
+    return object[property] ? true : false;
   //return true if the object has the value of the property argument
   //property is a string
   //otherwise return false
 }
 
+//ok
 function verifyPassword(user, password) {
+    return user.password === password ? true : false;
   //check to see if the provided password matches the password property on the user object
   //return true if they match
   //otherwise return false
 }
 
+//ok
 function updatePassword(user, newPassword) {
+    user.password = newPassword;
+    return user;
   //replace the existing password on the user object with the value of newPassword
   //return the object
 }
 
+//ok
 function addFriend(user, newFriend) {
+    user.friends.push(newFriend);
+    return user;
   //user has a property called friends that is an array
   //add newFriend to the end of the friends array
   //return the user object
 }
 
+//ok
 function setUsersToPremium(users) {
+    users.map(function(prop) {
+        prop.isPremium = true;
+    });
+    return users;
   //users is an array of user objects.
   //each user object has the property 'isPremium'
   //set each user's isPremium property to true
   //return the users array
 }
 
+//ok
 function sumUserPostLikes(user) {
+    var sum = 0;
+    user.posts.map(function(post) {
+        sum += post.likes;
+    });
+return sum;
+}
   //user has an array property called 'posts'
   //posts is an array of post objects
   //each post object has an integer property called 'likes'
   //sum together the likes from all the post objects
   //return the sum
-}
 
+
+
+
+
+
+//ok
 function addCalculateDiscountPriceMethod(storeItem) {
+    storeItem.calculateDiscountPrice = function(){
+      return this.price - (this.price * this.discountPercentage);
+    };
+    return storeItem;
+  }
   //add a method to the storeItem object called 'calculateDiscountPrice'
   //this method should multiply the storeItem's 'price' and 'discountPercentage' to get the discount
   //the method then subtracts the discount from the price and returns the discounted price
-  //example: 
+  //example:
   //price -> 20
   //discountPercentage -> .2
   //discountPrice = 20 - (20 * .2)
-}
+
 
 //Do not modify code below this line.
 ////--------------------------------
